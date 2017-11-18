68188049a38695ef91bb9d25aeb02a3c0ac2c96a
liubangchen <liubangchen@tencent.com>
2017-11-19 00:15:21 +0800

    for kafka-1.0.0

--- emr-example/src/main/scala/com/tencent/cloud/emr/KafakProducer.scala
+++ emr-example/src/main/scala/com/tencent/cloud/emr/KafakProducer.scala
@@ -13,9 +13,9 @@ import scala.util.Random
 
 object KafakProducer {
 
-  def getProps(brokers: String): Properties = {
+  def getProps(): Properties = {
     val props = new Properties()
-    props.put("bootstrap.servers", brokers)
+    props.put("bootstrap.servers","10.0.0.184:9092")
     props.put("client.id", "aProducerExample")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
@@ -27,10 +27,8 @@ object KafakProducer {
   }
 
   def main(args: Array[String]): Unit = {
-    val topic = args(1)
-    val brokers = args(2)
-    val rnd = new Random()
-    val props = getProps(brokers)
+    val topic = "testdataset"
+    val props = getProps()
 
     val producer = new KafkaProducer[String, String](props)
     val t = System.currentTimeMillis()
