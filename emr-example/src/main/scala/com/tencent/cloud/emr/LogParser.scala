0e3ee5859446e546ba2fb2527fc7073f0a65603e
liubangchen <liubangchen@tencent.com>
2017-11-22 17:08:48 +0800

    log parse

--- emr-example/src/main/scala/com/tencent/cloud/emr/LogParser.scala
+++ emr-example/src/main/scala/com/tencent/cloud/emr/LogParser.scala
@@ -124,7 +124,7 @@ object LogParser {
     }
     val df = sparksession.read.json(allrdd)
     df.repartition(1).write.mode(SaveMode.Append).format("ORC").saveAsTable("requestinfo_tmp")
-    var insertsql: String = "insert into table requestinfo partition (ds='" + bds.value + "') select * from requestinfo_tmp"
+    var insertsql: String = "insert into table requestinfo partition (ds='" + bds.value + "') select city,country,ip,appid,mobile,imsi,pvuid from requestinfo_tmp"
     sparksession.sql(insertsql)
     sparksession.sql("drop table requestinfo_tmp")
     fileSystem.close()
