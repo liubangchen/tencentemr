cb2a3402d344625f557d0990da98204cd0515c41
liubangchen <liubangchen@tencent.com>
2017-11-24 18:10:24 +0800

    ok

---
+++ qcloudapi-sdk-java/src/com/qcloud/Module/Emr.java
@@ -0,0 +1,8 @@
+package com.qcloud.Module;
+
+public class Emr extends Base {
+    //protected String serverHost = "cvm.api.qcloud.com";
+    public Emr(){
+        serverHost = "emr.api.qcloud.com";
+    }
+}
