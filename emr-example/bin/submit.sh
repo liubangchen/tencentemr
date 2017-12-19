5b28c6f4da6fc65713f4cc5c85e6c6f8190b01de
liubangchen <liubangchen@tencent.com>
2017-12-19 14:55:46 +0800

    ok

--- emr-example/bin/submit.sh
+++ emr-example/bin/submit.sh
@@ -2,21 +2,24 @@
 --master yarn \
 --deploy-mode cluster \
 --class  com.tencent.cloud.emr.CosLogParser \
---conf spark.locality.wait=10 \
---executor-memory 16g \
---num-executors  6  \
---executor-cores 4 \
---conf spark.shuffle.memoryFraction=0.9 \
---conf spark.storage.memoryFraction=0.1 \
---driver-memory 10g  \
+--conf spark.locality.wait=2 \
+--executor-memory 6g \
+--num-executors  20  \
+--executor-cores 1 \
+--conf spark.memory.fraction=0.7 \
+--conf spark.storage.storageFraction=0.1 \
+--driver-memory 4g  \
+--conf spark.shuffle.io.maxRetries=200 \
+--conf spark.shuffle.file.buffer=64k \
+--conf spark.shuffle.io.retryWait=15s \
 --conf spark.kryoserializer.buffer.max=512m \
 --conf spark.sql.crossJoin.enabled=true \
- --conf spark.default.parallelism=60 \
---conf spark.sql.shuffle.partitions=8 \
+--conf spark.default.parallelism=20 \
+--conf spark.sql.shuffle.partitions=20 \
 --conf spark.io.compression.codec=snappy \
 --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
 --conf spark.sql.inMemoryColumnarStorage.compressed=true \
 --conf spark.sql.inMemoryColumnarStorage.batchSize=10000 \
---conf "spark.executor.extraJavaOptions=-Xmn6g -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:GCPauseIntervalMillis=100" \
+--conf "spark.executor.extraJavaOptions=-Xmn2g -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:GCPauseIntervalMillis=100" \
 --conf "spark.driver.extraJavaOptions=-XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:GCPauseIntervalMillis=100"  \
 /usr/local/service/hadoop/emr-example-1.0-SNAPSHOT.jar /user/root/*/*.log /cos/output
