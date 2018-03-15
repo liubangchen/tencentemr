b3ce2045e693dee261c06791a9c080aeaca847af
liubangchen <liubangchen@tencent.com>
2018-03-15 20:58:53 +0800

    ok

--- emr-sdk/src/main/python/emrdemo.py
+++ emr-sdk/src/main/python/emrdemo.py
@@ -10,6 +10,7 @@ class EMRQcloudApi(QcloudApi):
     def _factory(self, module, config):
         return Emr(config)
 
+
 '''
 安装
 $ pip install qcloudapi-sdk-python
@@ -21,7 +22,15 @@ $ python setup.py install
 module = 'emr'
 
 '''
+参数说明详见
+https://cloud.tencent.com/document/api/589/10216
 action: 对应接口的接口名，请参考wiki文档上对应接口的接口名
+
+EmrDescribeCluster 查询集群列表
+EmrDescribeClusterNode 查询集群节点信息
+EmrScaleoutCluster 扩容接口
+EmrDestroyTaskNode 缩容接口
+
 '''
 action = 'EmrDescribeCluster'
 
@@ -29,11 +38,32 @@ action = 'EmrDescribeCluster'
 config: 云API的公共参数
 '''
 config = {
-    'Region': 'ap-shanghai',
-    'secretId': 'abc',
-    'secretKey': 'efg',
+    'Region': 'ap-beijing',
+    'secretId': '123',
+    'secretKey': '123',
+}
+'''
+查询集群信息参数
+{
+ 'ClusterId':'emr-123456',
+ 'NodeFlag':'all'
 }
 
+扩容参数
+{
+   'ClusterId':'emr-123456',
+   'CoreNodes':0,
+   'TaskNodes':1,
+   'NodeChargeType':0
+}
+
+缩容
+{
+  'ClusterId':'emr-123456',
+  'IPs.0':'192.168.0.1'
+}
+
+'''
 # 接口参数
 action_params = {
 
@@ -45,4 +75,5 @@ try:
     print(service.call(action, action_params))
 except Exception as e:
     import traceback
+
     print('traceback.format_exc():\n%s' % traceback.format_exc())
